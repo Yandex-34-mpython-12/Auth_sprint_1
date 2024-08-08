3250386bfc0849e5c1dececad6e042f91a5c80e4
vladislavbaluta <vladislav.baluta@itglobal.com>
2024-08-08 14:48:06 +1000

    add permission tables

--- auth_service/src/models/token.py
+++ auth_service/src/models/token.py
@@ -1,7 +1,7 @@
 import uuid
 from datetime import datetime
 
-from sqlalchemy import DateTime, func, ForeignKey
+from sqlalchemy import DateTime, func, ForeignKey, BigInteger
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from src.db.postgres import Base
@@ -11,7 +11,7 @@ from src.models.mixins import UserSchemaMixin
 class Token(UserSchemaMixin, Base):
     __tablename__ = 'tokens'
 
-    id: Mapped[uuid.UUID] = mapped_column(primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
+    id: Mapped[int] = mapped_column(BigInteger, primary_key=True)
     refresh_token: Mapped[str] = mapped_column(unique=True, nullable=False)
     user_id: Mapped[uuid.UUID] = mapped_column(ForeignKey('users.users.id'), unique=True)
     created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
--- auth_service/src/models/user.py
+++ auth_service/src/models/user.py
@@ -1,12 +1,22 @@
 import uuid
 from datetime import datetime
+from typing import List
 
-from sqlalchemy import DateTime, String, func, ForeignKey
+from sqlalchemy import DateTime, String, func, ForeignKey, Table, Column
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from src.db.postgres import Base
 from src.models.mixins import UserSchemaMixin
 from src.utils.hash import verify_password, hash_password
+
+
+user_permissions_table = Table(
+    "user_permissions",
+    Base.metadata,
+    Column("user_id", ForeignKey("users.users.id"), primary_key=True),
+    Column("permission_id", ForeignKey("users.permissions.id"), primary_key=True),
+    schema='users',
+)
 
 
 class User(UserSchemaMixin, Base):
@@ -17,9 +27,14 @@ class User(UserSchemaMixin, Base):
     password: Mapped[str] = mapped_column(String(255), nullable=False)
     first_name: Mapped[str] = mapped_column(String(50))
     last_name: Mapped[str] = mapped_column(String(50))
+    is_staff: Mapped[bool] = mapped_column(default=False)
+    is_active: Mapped[bool] = mapped_column(default=True)
     created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
 
     token: Mapped["Token"] = relationship(back_populates="user")
+    permissions: Mapped[List["Permission"]] = relationship(
+        secondary=user_permissions_table, back_populates="users"
+    )
 
     def __init__(self, **kwargs) -> None:
         password = kwargs.pop('password')
@@ -31,3 +46,18 @@ class User(UserSchemaMixin, Base):
 
     def __repr__(self) -> str:
         return f'<User {self.login}>'
+
+
+class Permission(UserSchemaMixin, Base):
+    __tablename__ = 'permissions'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
+    codename: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
+
+    users: Mapped[List["User"]] = relationship(
+        secondary=user_permissions_table, back_populates="permissions"
+    )
+
+    def __repr__(self) -> str:
+        return f'<Permission {self.name}>'
