12ddab9df5baae35c439e2178ddbbc1c756423aa
vladislavbaluta <vladislav.baluta@itglobal.com>
2024-08-11 20:11:25 +1000

    refactor get_users_db

--- auth_service/src/api/dependencies/authentication/users.py
+++ auth_service/src/api/dependencies/authentication/users.py
@@ -1,9 +1,10 @@
 from typing import TYPE_CHECKING, Annotated
 
 from fastapi import Depends
+from fastapi_users_db_sqlalchemy import SQLAlchemyUserDatabase
+
 from src.db.postgres import db_helper
 from src.models import User
-from src.services.auth import AuthUserService
 
 if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
@@ -15,4 +16,4 @@ async def get_users_db(
         Depends(db_helper.session_getter),
     ],
 ):
-    yield AuthUserService(session, User)
+    yield SQLAlchemyUserDatabase(session, User)
