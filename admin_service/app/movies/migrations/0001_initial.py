560eca2d8a70fd2beeae0453da037d854a511289
Ren22 <ngm.renat@gmail.com>
2024-08-18 03:30:27 +0200

    feat(sprint 6+7): disable certain services for now;
    - fix small issues for django containers
    - provide .enb.example files

--- admin_service/app/movies/migrations/0001_initial.py
+++ admin_service/app/movies/migrations/0001_initial.py
@@ -2,9 +2,11 @@ from django.db import migrations
 
 
 class Migration(migrations.Migration):
-    initial = True
 
     dependencies = [
+        ('custom_user', '0001_initial'),  # Ensure this line is present
+        ('auth', '0001_initial'),
+        ('contenttypes', '0001_initial'),
     ]
 
     operations = [
